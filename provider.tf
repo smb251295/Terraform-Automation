07cd94f4cbe2ac9e936a819efaa1e87cc0942e26
smb251295 <127755157+smb251295@users.noreply.github.com>
2023-07-29 22:49:56 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  access_key = "AKIA3H4W24M5EG65TPPE"
+  secret_key = "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"
 }
 
 provider "aws" {
